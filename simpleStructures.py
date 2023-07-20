388f15540de248d12955d81abc820e8907e54337
KlassnayaAfrodita <113383200+KlassnayaAfrodita@users.noreply.github.com>
2023-07-20 17:25:37 +0300

    Update simpleStructures.py

--- simpleStructures.py
+++ simpleStructures.py
@@ -22,52 +22,57 @@ class Stack:
         self.head = StackNode(self.head.next.next.value, self.head.next.next) #1st arg -> None
         return None
 
+
 # QUEUE
-# class QueueNode:
-#     def __init__(self, value, prev):
-#         self.value = value
-#         self.prev = prev
+class QueueNode:
+    def __init__(self, value, node):
+        self.value = value
+        self.node = node
 
 # class Queue:
 #     def __init__(self):
 #         self.front = QueueNode(None, None)
-#         self.back = QueueNode(None, self.back)
-    
+#         self.back = QueueNode(None, self.front)
+        
 #     def top(self):
-#         return QueueNode(self.front.prev.value, self.front.prev)
-
-#     def push(self, value):
-#         newNode = QueueNode(value, self.back.prev)
-#         self.back = QueueNode(None, newNode)
-#         self.back.prev = QueueNode(self.back.prev.value, newNode)
-
-#     def pop(self):
-#         self.front.prev.prev = QueueNode(self.front.prev.prev, self.front)
-        # return None
-# class QueueNode:
-#     def __init__(self, value, next):
-#         self.value = value
-#         self.next = next
-
-# class Queue:
-#     def __init__(self):
-#         self.back = QueueNode(None, None)
-#         self.front = QueueNode(None, self.back)
-    
-#     def top(self):
-#         return QueueNode(self.front.next.value, self.next.next)
+#         return QueueNode(self.front.node.value, self.front.node) #if self.front.node != None else QueueNode(None, )
 
 #     def push(self, value):
 #         newNode = QueueNode(value, self.back)
-#         if self.back.next != None:
-#             self.back.next = QueueNode(self.back.next.value, newNode)
-#         else: self.back.next = QueueNode(None, newNode)
+#         if self.back.node.node: self.back.node = QueueNode(self.back.node.value, newNode)
+#         else: self.front = QueueNode(self.back.node.value, newNode)
+#         self.back = QueueNode(None, newNode)
+        
 #     def pop(self):
-#         self.front.prev.prev = QueueNode(self.front.prev.prev, self.front)
+#         self.front = QueueNode(self.front.node.node.value, self.front.node.node)
+#         return None
+
+class Queue:
+    def __init__(self):
+        self.back = QueueNode(None, None)
+        self.front = QueueNode(None, self.back)
+        
+    def top(self):
+        return QueueNode(self.front.node.value, self.front.node) #if self.front.node != None else QueueNode(None, )
+
+    def push(self, value):
+        newNode = QueueNode(value, self.back.node)
+        # if self.back.node.node: self.back.node = QueueNode(self.back.node.value, newNode)
+        # else: self.front = QueueNode(self.back.node.value, newNode)
+        self.back = QueueNode(None, newNode)
+        
+    def pop(self):
+        self.front = QueueNode(self.front.node.node.value, self.front.node.node)
+        return None
 
 a = Queue()
 a.push(1)
-print(a.top().value)
-# a.push(2)
-# a.pop()
+a.push(2)
+# print(a.top().value)
+print(a.front.node.value)
+print(a.back.node.value)
+a.pop()
+a.push(3)
+print(a.front.node.value)
+print(a.back.node.value)
 # print(a.top().value)
